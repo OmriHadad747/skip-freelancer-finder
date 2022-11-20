45319b31bc010cf4bf8b53756c8193986ff0f21c
OmriHadad747 <omrihadad747@gmail.com>
2022-11-20 18:09:37 +0200

    1

--- app/services/finder.py
+++ app/services/finder.py
@@ -18,50 +18,76 @@ from skip_db_lib.database.customers import CustomerDatabase as customers_db
 class FreelancerFinder:
     @staticmethod
     def _exclude_failed_tokens(tokens: List[str], resps: List[messaging.SendResponse]) -> List[str]:
-        # TODO write docstring
-        failed_tokens = [tokens[idx] for idx, resp in enumerate(resps) if not resp.success]
+        """Finds the failed registration tokens in 'resps' and remove
+        them from database cause they are proably invalid.
 
+        Args:
+            tokens (List[str]): Registration tokens that a notification pushed to.
+            resps (List[messaging.SendResponse]): List of responses from each notification for each freelancer notified.
+
+        Returns:
+            List[str]: List of all the registration tokens that actually notified
+        """
+        failed_tokens = [tokens[idx] for idx, resp in enumerate(resps) if not resp.success]
         app.logger.debug(f"discarding invalid registration tokens {failed_tokens}")
 
         # TODO implement the call to the db function that remove the registration token from freelancers
-
-        return [tokens.remove(t) for t in failed_tokens if t in tokens]
+        # implement here during testing with real registration tokens
+        
+        return [t for t in failed_tokens if t not in tokens]
 
     @classmethod
     def _nofity_freelancers(
         cls, job: job_model.Job, freelancers: command_cursor.CommandCursor
     ) -> List[str]:
-        # TODO write docstring
+        """Found the registration token for each freelancer, and eventually
+        trying to push notification for all of them. 
+
+        If there are failures with some registation tokens, deleting them frm
+        database.
+
+        Args:
+            job (job_model.Job): A job to notify freelancers about.
+            freelancers (command_cursor.CommandCursor): Cursor of available freelancers
+
+        Returns:
+            List[str]: List of all the registration tokens that actually notified
+        """
         app.logger.info("notifying freelancers about incoming job")
 
         tokens = [f.get("registration_token") for f in freelancers]
 
         msg = messaging.MulticastMessage(data=job.job_to_str(), tokens=tokens)
         resp: messaging.BatchResponse = messaging.send_multicast(msg, dry_run=True)
-        if resp.failure_count > 0:
-            return cls._exclude_failed_tokens(tokens, resp.responses)
+        # TODO unfreeze here when working with real registration tokens
+        # if resp.failure_count > 0:
+        #     return cls._exclude_failed_tokens(tokens, resp.responses)
 
-        app.logger.debug(f"{resp.success_count} notified | {resp.failure_count} not notified")
+        app.logger.debug(f"from {tokens} | {resp.success_count} notified | {resp.failure_count} not notified")
         return tokens
 
     @classmethod
     @pyd.validate_arguments
     def _notify_customer(cls, job: job_model.Job, customer: customer_model.Customer) -> None:
-        # TODO write docstring
+        """Notify a customer that a freelancer has been found for his job
+
+        Args:
+            job (job_model.Job): Customer's job.
+            customer (customer_model.Customer): Customer to notify.
+        """
         app.logger.info("notifying customer that a freelancer was found")
 
         msg = messaging.Message(
             data=job.job_to_str(freelancer_part=True), token=customer.registration_token
         )
-        resp = messaging.send(msg, dry_run=True)
-
+        # resp = messaging.send(msg, dry_run=True)
+        resp = None
         app.logger.debug(f"customer notified with message {resp}")
 
     @classmethod
     @middlewares.save_incoming_job
     def find(cls, incoming_job: job_model.Job) -> Tuple[flask.Response, int]:
-        """
-        Find available and nearest freelancers to the job location
+        """Find available and nearest freelancers to the job location
         (which is actually the customer location) using skip-db-lib.
 
         Args:
@@ -89,8 +115,7 @@ class FreelancerFinder:
     @classmethod
     @middlewares.update_incoming_job
     def take(cls, job_id: str = None) -> Tuple[flask.Response, int]:
-        """
-        In case the given 'job_id' equals None, you can assume that the job already
+        """In case the given 'job_id' equals None, you can assume that the job already
         taken by another freelancer.
 
         Otherwise, fetch the job and corresponded customer from the database
